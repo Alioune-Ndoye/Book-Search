31a8193e07e86393e6fbbe60f8afa12152109460
Alioune-Ndoye <alithephoenix7@gmail.com>
2025-03-06 17:29:35 -0500

    Apptsx update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,7 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
+import {ApolloClient,InMemoryCache,ApolloProvider,createHttpLink,} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-
 import Navbar from './components/Navbar';
 
 // Define the main GraphQL API endpoint
